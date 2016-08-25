74937345fc0ec9a468e10ad89b20f9c4a5f454d0
Jason Yeo <jasonyeo88@gmail.com>
2016-08-25 10:53:03 +0800

    Remove pycrypto and feedparser

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from jwt import algorithms
 from django.utils import formats
 from rsa import cli
-from feedparser import feedparser
 from requests import sessions
-from lib.Crypto.PublicKey import ElGamal
+
 if __name__ == '__main__':
     formats.get_format()
     algorithms.HMACAlgorithm.prepare_key()
     cli.VerifyOperation.perform_operation()
-    feedparser.parse()
     sessions.SessionRedirectMixin.resolve_redirects()
-    ElGamal.generate()
